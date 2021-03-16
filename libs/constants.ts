131a518cd4cd4c6e7e074a8bba2c6c92ad902ca6
kokily <kokily@naver.com>
2021-03-16 12:36:20 +0900

    Production Server name

--- libs/constants.ts
+++ libs/constants.ts
@@ -1,3 +1,3 @@
 export const isProd = process.env.NODE_ENV === 'production';
 export const devServer = 'http://localhost:4000';
-export const prodServer = 'https://thewhy.kr';
+export const prodServer = 'https://api.thewhy.kr';
