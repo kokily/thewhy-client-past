bf913ecc3da7f484ca07a2fa712c589bd64ed81d
kokily <kokily@naver.com>
2021-03-25 15:15:51 +0900

    Add Question Permission modify

--- pages/question/add.tsx
+++ pages/question/add.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
 import PageTemplate from '../../components/common/PageTemplate';
 import AddContainer from '../../containers/question/AddContainer';
-import useAdmin from '../../libs/hooks/useAdmin';
 
 function AddQuestionPage() {
-  useAdmin();
-
   return (
     <PageTemplate>
       <AddContainer />
