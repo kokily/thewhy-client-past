1d5ffa5ee7b759c78eb747ee3cecdcd41ae0fea2
kokily <kokily@kokilyui-MacBookPro.local>
2021-05-15 16:38:44 +0900

    naver Meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -33,55 +33,7 @@ class MyDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <meta charSet="utf-8" />
-          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-
-          <meta
-            name="keywords"
-            content="커뮤니케이션, 교육, 강의, 더와이컨설팅, 더와이, The Y ConsulTing, Y"
-          />
-          <meta
-            name="description"
-            content="더와이컨설팅은 개인과 조직의 행복한 관계를 꿈꾸는 커뮤니케이션 교육 전문기업입니다"
-          />
-          <meta name="author" content="thewhy.kr" />
-          <meta property="og:title" content="더와이컨설팅" />
-          <meta
-            property="og:description"
-            content="더와이컨설팅은 개인과 조직의 행복한 관계를 꿈꾸는 커뮤니케이션 교육 전문기업입니다"
-          />
-          <meta property="og:image" content="/img/main-logo.png" />
-
-          <link rel="shortcut icon" href="/img/favicon.ico" type="iamge/x-icon" />
-          <link rel="apple-touch-icon" href="/img/apple-touch-icon.png" />
-
-          <link
-            id="googleFonts"
-            href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700,800%7CShadows+Into+Light&display=swap"
-            rel="stylesheet"
-            type="text/css"
-          />
-
-          <link rel="stylesheet" href="/css/bootstrap.min.css" />
-          <link rel="stylesheet" href="/css/all.min.css" />
-          <link rel="stylesheet" href="/css/animate.compat.css" />
-          <link rel="stylesheet" href="/css/simple-line-icons.min.css" />
-          <link rel="stylesheet" href="/css/owl.carousel.min.css" />
-          <link rel="stylesheet" href="/css/owl.theme.default.min.css" />
-          <link rel="stylesheet" href="/css/magnific-popup.min.css" />
-
-          <link rel="stylesheet" href="/css/theme.css" />
-          <link rel="stylesheet" href="/css/theme-elements.css" />
-          <link rel="stylesheet" href="/css/theme-blog.css" />
-          <link rel="stylesheet" href="/css/theme-shop.css" />
-
-          <link rel="stylesheet" href="/css/component.css" />
-          <link id="skinCSS" rel="stylesheet" href="/css/default.css" />
-          <link rel="stylesheet" href="/css/custom.css" />
-          <script src="/js/modernizr.min.js"></script>
-          <script src="//dapi.kakao.com/v2/maps/sdk.js?appkey=cec30f2d8f996a53cca24b45925dea37&libraries=services"></script>
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
