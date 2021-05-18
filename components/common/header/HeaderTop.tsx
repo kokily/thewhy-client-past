bb88f07d31e8d90a8d1e3a8ad01b53a0847b898b
kokily <kokily@naver.com>
2021-05-18 12:28:38 +0900

    SSLify

--- components/common/header/HeaderTop.tsx
+++ components/common/header/HeaderTop.tsx
@@ -45,7 +45,7 @@ function HeaderTop() {
     <div className="header-column justify-content-start justify-content-md-center order-1 order-md-2">
       <CenterLogo className="header-row">
         <div className="header-logo">
-          <a href="/">
+          <a href="http://thewhy.kr/">
             <img
               alt="Porto"
               width="200"
